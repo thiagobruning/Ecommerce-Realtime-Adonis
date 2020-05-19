43a2211624174f0ece8a097c6ebceea60bab368f
Thiago Bruning <thi.bruning@hotmail.com>
2020-05-18 22:40:00 -0300

    Add Mail package

--- start/app.js
+++ start/app.js
@@ -11,12 +11,13 @@
 |
 */
 const providers = [
-  '@adonisjs/framework/providers/AppProvider',
-  '@adonisjs/auth/providers/AuthProvider',
-  '@adonisjs/bodyparser/providers/BodyParserProvider',
-  '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/lucid/providers/LucidProvider'
-]
+  "@adonisjs/framework/providers/AppProvider",
+  "@adonisjs/auth/providers/AuthProvider",
+  "@adonisjs/bodyparser/providers/BodyParserProvider",
+  "@adonisjs/cors/providers/CorsProvider",
+  "@adonisjs/lucid/providers/LucidProvider",
+  "@adonisjs/mail/providers/MailProvider",
+];
 
 /*
 |--------------------------------------------------------------------------
