75890c1b53cb485d5bac3850f3419df04c33760c
Thiago Bruning <thi.bruning@hotmail.com>
2020-06-17 17:43:26 -0300

    Implemented client register method

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,10 +1,30 @@
 'use strict'
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
 
 class AuthController {
 
   async register({ request, response })
   {
-    //
+    /**
+     * Using transactions to avoid errors in database, and rollback the request
+     * when occur a error
+     */
+    const trx = await Database.beginTransaction()
+
+    try {
+      const { name, surname, email, password } = request.all()
+      const user = await User.create({ name, surname, email, password }, trx)
+      const clientRole = await Role.findBy('slug', 'client')
+      await user.roles().attach([clientRole.id], null, trx)
+      await trx.commit()
+
+      return response.status(201).send({ data: user })
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({ message: 'Erro ao realizar cadastro.' })
+    }
   }
 
   async login({ auth, request, response })
