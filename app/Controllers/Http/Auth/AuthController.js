8236ab18e8636c3a279eec6aa0981d3c4e573855
Thiago Bruning <thi.bruning@hotmail.com>
2020-06-17 18:10:04 -0300

    Implemented refresh token method

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -38,7 +38,18 @@ class AuthController {
   /** Refresh token */
   async refresh({ request, response })
   {
-    //
+    /**
+     * if refresh not passed through body,
+     * take through header
+     */
+    var refreshToken = request.input('refresh_token')
+    if(!refreshToken) {
+      refreshToken = request.header('refresh_token')
+    }
+
+    const user = await auth.newRefreshToken().generateForRefreshToken(refreshToken)
+
+    return response.send({ data:user })
   }
 
   async logout({ auth, request, response })
