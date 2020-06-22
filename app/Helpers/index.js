990649995398e7d17405c0cd6fa983a871ca71bf
Thiago Bruning <thi.bruning@hotmail.com>
2020-06-22 15:11:19 -0300

    Created helper method to make random strings

---
+++ app/Helpers/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Crypto = use('Crypto')
+const Helpers = use('Helpers')
+
+
+/**
+ * Helper to generate random string.
+ * Use in path name images, generate tokens
+ * @param { int } length
+ * @return { string } - the random string
+*/
+
+const str_random = async (length = 40) => {
+    let string = ''
+    let len = string.length
+
+    if(len < length) {
+        let size = length - len
+        let bytes = await Crypto.randomBytes(size)
+        let buffer = new Buffer(bytes)
+        string += buffer
+            .toString('base64')
+            .replace(/[^a-zA-Z]/g, '')
+            .substr(0, size)
+    }
+    return string
+}
+
+module.exports = {
+    str_random
+}
